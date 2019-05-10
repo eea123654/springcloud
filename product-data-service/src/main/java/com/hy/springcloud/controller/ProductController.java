8a5586fa60c853b77b684faa00988cddf66481d5
eea123654 <784827222@qq.com>
2019-05-10 17:37:14 +0800

    begin

---
+++ product-data-service/src/main/java/com/hy/springcloud/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.hy.springcloud.controller;
+
+import java.util.List;
+
+import com.hy.springcloud.pojo.Product;
+import com.hy.springcloud.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @RequestMapping("/products")
+    public Object products() {
+        List<Product> ps = productService.listProducts();
+        return ps;
+    }
+}
